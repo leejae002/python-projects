c7c383ebc4208a6b51b9dd85a6ba0c31533809af
Jaechan Lee <ljae0724@gmail.com>
2020-03-20 14:00:22 -0700

    Practice with for and while loops

---
+++ Loop_practice.py
@@ -0,0 +1,15 @@
+'''
+ingredients = ['bread', 'soup', 'onion', 'grape', 'milk', 'carrot']
+
+for x in range (1,6):
+    print(x, ingredients[x])
+
+'''
+
+weight = 50
+
+for years in range (0,15):
+    weight = weight + 1
+    moon_weight = weight * 0.165
+    print(moon_weight)
+    
