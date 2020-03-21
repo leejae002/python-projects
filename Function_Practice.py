0809e309286f3cd2d695df57c9042bd95d703900
Jaechan Lee <ljae0724@gmail.com>
2020-03-20 21:57:36 -0700

    Practice with return statement

---
+++ Function_Practice.py
@@ -0,0 +1,26 @@
+'''def greeting(name):
+    print('Hello %s' % name)
+
+greeting('Joe')
+
+greeting('Hand Sanitizer')
+'''
+
+#more complex?
+
+'''
+def greeting(name1, name2):
+    print('Hello %s %s' % (name1, name2))
+
+greeting('Bob', 'Jones')
+'''
+
+def test():
+    one = 100
+    two = 200
+    return one * two
+
+print(test())
+
+
+
