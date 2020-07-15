de28b63e2786839d254009062f9a3b054281963c
Jaechan Lee <ljae0724@gmail.com>
2020-07-15 15:13:39 -0700

    I don't remember what this was supposed to be

---
+++ Weight_thing.py
@@ -0,0 +1,16 @@
+weight1 = 6
+weight2 = 5
+
+def value_1(weight):
+    weight1 = weight * 6
+    return weight * 6
+def value_2(weight):
+    weight2 = weight * 5
+    return weight * 5
+
+if weight1 > weight2:
+    print("yes")
+else:
+    print("no")
+
+
