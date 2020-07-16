8e2d4bbd4787751978a07d219f1032377ccb3a39
Jaechan Lee <ljae0724@gmail.com>
2020-07-16 10:28:36 -0700

    Fixed bug with scissors

--- Rock.Paper.Scissors.py
+++ Rock.Paper.Scissors.py
@@ -8,12 +8,12 @@ def rps_game():
         player_int = rps_lst.index(player)
         cpu_shoot = (rps_lst[cpu_int])
         if cpu_int == player_int:
-            print("We both played " + rps_lst[player])
+            print("We both played " + rps_lst[player_int])
     
-        if cpu_int == 1 and player_int == 3:
-            print("I played " + rps_lst[1] + ". You lose!")
-        elif cpu_int == 3 and player_int == 1:
-            print("I played " + rps_lst[3] + ". You win!")
+        elif cpu_int == 0 and player_int == 2:
+            print("I played " + rps_lst[0] + ". You win!")
+        elif cpu_int == 2 and player_int == 0:
+            print("I played " + rps_lst[2] + ". You lose!")
         elif int(cpu_int) > int(player_int):
             print("I played " + cpu_shoot + ". You lose!")
         else:
